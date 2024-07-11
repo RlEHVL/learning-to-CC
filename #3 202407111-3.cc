4e30f1e9926eb881a2eb5fd85d43251e799f8c36
RlEHVL <165342717+RlEHVL@users.noreply.github.com>
2024-07-12 00:29:06 +0900

    Update #3 202407111-3.cc

--- #3 202407111-3.cc
+++ #3 202407111-3.cc
@@ -180,3 +180,42 @@ int main(void) {
     }
     return 0;
 }
+
+/**************************
+
+
+other solution on down
+
+
+**************************/
+
+
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int n;
+
+    cin >> n;
+
+    cout << n << " is " << (n & 1 ? "odd" : "even") << endl; // ? keyword
+
+    return 0;
+}
